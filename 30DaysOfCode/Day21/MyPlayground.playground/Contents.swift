07a3459cb91a76170bd161dbbb72804122f35e17
Kenta Kodashima <kentakodashima@gmail.com>
2019-03-24 21:44:54 -0700

    Add Day 21.

---
+++ 30DaysOfCode/Day21/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+struct Printer<T> {
+  /**
+   *    Name: printArray
+   *    Print each element of the generic array on a new line. Do not return anything.
+   *    @param A generic array
+   **/
+  
+  // Write your code here
+  func printArray(array: [T]) {
+    for element in array {
+      print(element)
+    }
+  }
+}
